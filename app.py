8910ab49d930f2bee5d0e5d0d5dff3ecb7fb7038
Martin Urbanec <martin@urbanec.cz>
2017-04-22 22:31:15 +0200

    Move static files to static folder, serve static files

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 #import logic.getData as getData
 import logic.dbStreamer as dbStreamer
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
 
 @app.route('/')
 def homepage():
-    card_UID = "101010" #This is a testvalue
-    return render_template("index.html", title='SimpleAttendanceSystem', cardUID=card_UID)
+	card_UID = "101010" #This is a testvalue
+	return render_template("index.html", title='SimpleAttendanceSystem', cardUID=card_UID)
+
+@app.route('/js/<path:path>')
+def sendJS(path):
+	return send_from_directory('static/js', path)
+
+@app.route('/css/<path:path>')
+def sendCSS(path):
+	return send_from_directory('static/css', path)
 
 if __name__ == "__main__":
-    app.run()
+	app.run()
