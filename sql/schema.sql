7609e5639cf544b429b744959b039cc2ac9134bc
Tomas Pelak <s3w3nofficial@gmail.com>
2017-04-22 22:54:04 +0200

    sql schema edited

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,3 +1,6 @@
+#define users
 drop table users if exists;
 create table users ( id int not null auto_increment, primary key(id) , name varchar(255) not null, card_id int not null );
-#put there definition for istream
+#define istream
+drop table istream if exists;
+create table istream ( id int not null auto_increment, primary key(id), card_id int not null, time time not null);
