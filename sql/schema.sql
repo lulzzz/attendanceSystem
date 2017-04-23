449babc489b585e8a879fb4fb3f7e57b6d153b7c
Martin Urbanec <martin@urbanec.cz>
2017-04-23 19:52:31 +0200

    Time is varchar(14)

--- sql/schema.sql
+++ sql/schema.sql
@@ -3,4 +3,4 @@ drop table if exists users;
 create table users ( id int not null auto_increment, primary key(id) , name varchar(255) not null, card_id int not null );
 #define istream
 drop table if exists istream;
-create table istream ( id int not null auto_increment, primary key(id), card_id int not null, time time not null );
+create table istream ( id int not null auto_increment, primary key(id), card_id int not null, time varchar(14) not null );
