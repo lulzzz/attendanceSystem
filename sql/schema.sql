025c1c349c7a51d70ec5f8a65aa1ccd9e1a56770
Martin Urbanec <martin@urbanec.cz>
2017-04-23 19:37:39 +0200

    Fix schema

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,6 +1,6 @@
 #define users
-drop table users if exists;
+drop table if exists users;
 create table users ( id int not null auto_increment, primary key(id) , name varchar(255) not null, card_id int not null );
 #define istream
-drop table istream if exists;
+drop table if exists istream;
 create table istream ( id int not null auto_increment, primary key(id), card_id int not null, time time not null );
