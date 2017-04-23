ee17fa8d8de2dd57dcc7c391c249b40a35dba2f4
Martin Urbanec <martin@urbanec.cz>
2017-04-23 20:02:32 +0200

    Generate used timestamp in app
    Do not allow users to say when they entered... It could be dangerous

--- logic/dbStreamer.py
+++ logic/dbStreamer.py
@@ -1,6 +1,7 @@
 import oursql
 import yaml
 import os
+import datetime
 
 __dir__ = os.path.dirname(__file__)
 config = yaml.safe_load(open(__dir__ + '/../config.yaml'))
@@ -34,11 +35,39 @@ def add_user(name, card_id):
 	with cur:
 		cur.execute('''insert into users (id, name, card_id) values (NULL, "%s", "%s");''' % (name, card_id), plain_query=True)
 
-def save_iStream(card_id, time):
+def save_iStream(card_id):
 	conn = connect()
+	now = datetime.datetime.now()
+	year = str(now.year)
+	month = now.month
+	if month < 10:
+		month = "0" + str(now.month)
+	else:
+		month = str(now.month)
+	day = now.day
+	if day < 10:
+		day = "0" + str(day)
+	else:
+		day = str(day)
+	hour = now.hour
+	if hour < 10:
+		hour = "0" + str(hour)
+	else:
+		hour = str(hour)
+	minute = now.minute
+	if minute < 10:
+		minute = "0" + str(minute)
+	else:
+		minute = str(minute)
+	second = now.second
+	if second < 10:
+		second = "0" + str(second)
+	else:
+		second = str(second)
+	timestamp = year + month + day + hour + minute + second
 	cur = conn.cursor()
 	with cur:	
-		cur.execute('''insert into istream (id, card_id, time) values (NULL, "%s", "%s");''' % (card_id, time), plain_query=True)
+		cur.execute('''insert into istream (id, card_id, time) values (NULL, "%s", "%s");''' % (card_id, timestamp), plain_query=True)
 
 #add_user("TestUser", "123456789")
-#save_iStream("123456789", "12:45")
+#save_iStream("123456789")
