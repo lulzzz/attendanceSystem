04940f9b9e67ff2f4d832caff1f8215fc2eb0b18
Martin Urbanec <martin@urbanec.cz>
2017-04-27 21:51:59 +0200

    Enable debug

--- app.py
+++ app.py
@@ -13,20 +13,20 @@ def homepage():
 	cvt = dbStreamer.complete_view_table()
 	data = {}
 	#data = { row1: [user, card_id time] }
-	
+
 	user_len = dbStreamer.calc_users()
 	user_len = user_len[0][0]
 
 	for user in users:
         	user = user[0]
-		
+
 		day_time = tp.calc_day_time(cvt[user]['times'][0], cvt[user]['times'][1], cvt[user]['times'][2], cvt[user]['times'][3])
 
 		time1 = tp.return_time_in_normal_form(str(cvt[user]['times'][0]))
 		time2 = tp.return_time_in_normal_form(str(cvt[user]['times'][1]))
 		time3 = tp.return_time_in_normal_form(str(cvt[user]['times'][2]))
 		time4 = tp.return_time_in_normal_form(str(cvt[user]['times'][3]))
-			
+
         	data[user] = [ str(user), str(cvt[user]['card_id']), str(cvt[user]['branch']), time1, time2, time3, time4, day_time ]
 
 	return render_template("index.html", title='SimpleAttendanceSystem', info=info, cvt=data, user_len=int(user_len))
@@ -40,4 +40,4 @@ def sendCSS(path):
 	return send_from_directory('static/css', path)
 
 if __name__ == "__main__":
-	app.run()
+	app.run(debug=True)
